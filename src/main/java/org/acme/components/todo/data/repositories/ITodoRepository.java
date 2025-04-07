e87b9e62ad8b12693bbffa88e32767f90080a327
fernando.rosag11 <fernando.rosag11@gmail.com>
2025-04-07 18:15:02 -0300

    init.

--- src/main/java/org/acme/components/todo/data/repositories/ITodoRepository.java
+++
@@ -1,7 +0,0 @@
-package org.acme.components.todo.data.repositories;
-
-import org.acme.components.todo.domain.entity.Todo;
-
-public interface ITodoRepository {
-    Boolean insertTodo(Todo todo);
-}
--- src/main/java/org/acme/components/todo/domain/usecase/ICreateTodo.java
+++
@@ -1,9 +0,0 @@
-package org.acme.components.todo.domain.usecase;
-
-public interface ICreateTodo {
-    
-    Output execute(Input input);
-
-    record Input(String name, String limitDate) {}
-    record Output(String message) {}
-}
